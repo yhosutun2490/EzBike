5fc742e8be67804606b28565893ebed9fbbb7b68
yhosutun2491 <yhosutun2491@gmail.com>
2023-01-06 16:27:40 +0800

    feat- add Ubike marker to GoogleMap

--- pages/search/index.js
+++ pages/search/index.js
@@ -6,33 +6,57 @@ import {
   Marker
 } from "@react-google-maps/api";
 
-function SearchPage () {
+function SearchPage (props) {
+  // ubike 資訊
+   const allBikesData = props.allBikesData
   // 先用javascript CSR載地圖
    const { isLoaded } = useLoadScript({
     // Enter your own Google Maps API key
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
   });
+
   return (
   <div className={styles["main"]}>
     <div>
        <SearchNavBar/>
     </div>
     <div className={styles["map"]}>
-      {!isLoaded ? <p>Loading....</p> : <Map/>}
+      {!isLoaded ? <p>Loading....</p> : <Map bikesData={allBikesData}/>}
     </div>
   </div>
   )
 
 }
 
-function Map () {
+function Map (props) {
+  const {bikesData} = props
+  const center = {lat:25.04792,lng:121.51741}
+
   return (
      <GoogleMap 
-     zoom={15} 
-     center={{lat:25.04792,lng:121.51741}} 
+     zoom={16} 
+     center={center } 
      mapContainerClassName={styles["map-container"]}>
+      <Marker position={center}/>
+      {bikesData.map( item=> <Marker key={item.sno}position={{lat:item.lat,lng:item.lng}}/>)}
   </GoogleMap>
   )
  
 }
+// sever SSG fetch data
+export async function getStaticProps () {
+  const allBikesData=  await fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+  return {
+    props: {
+      allBikesData: allBikesData
+    }
+  } 
+}
+
 export default SearchPage
