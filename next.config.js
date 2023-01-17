74918aa0e41b6af54166a4c2995612f821d8cdb3
yhosutun2491 <yhosutun2491@gmail.com>
2023-01-17 20:56:12 +0800

    fix- remove api key from config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//    env: {
-//     NEXT_PUBLIC_Google_Map_Api_Key: 'AIzaSyDBrrDTQzS8P-bTGDr6JiOIyjyAH5E3mec',
-//   },
-// }
 
-module.exports = { 
-  env: {
-    NEXT_PUBLIC_Google_Map_Api_Key: 'AIzaSyDBrrDTQzS8P-bTGDr6JiOIyjyAH5E3mec',
-  },
-}
+
