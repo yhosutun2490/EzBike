4f84767c8dd1b5e53bcf65c95b02066f02b22f17
yhosutun2491 <yhosutun2491@gmail.com>
2023-02-02 11:32:47 +0800

    rename userGeoLocation api

--- pages/api/broswerUserGeolocationApi.js
+++ pages/api/broswerUserGeolocationApi.js
@@ -1,4 +1,4 @@
-const userGeolocationApi =  new Promise(function(resolve, reject) {
+const broswerUserGeolocationApi =  new Promise(function(resolve, reject) {
     // 如果使用者的瀏覽器有支援geolocation，回傳本身定位
     if (navigator.geolocation && navigator.geolocation.getCurrentPosition ) {
       navigator.geolocation.getCurrentPosition((result)=> resolve({lat:result.coords.latitude,lng:result.coords.longitude},
@@ -16,4 +16,4 @@ const userGeolocationApi =  new Promise(function(resolve, reject) {
 
 
 
-export default userGeolocationApi
+export default broswerUserGeolocationApi
